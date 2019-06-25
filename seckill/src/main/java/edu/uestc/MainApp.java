153226e05484dad0cdebb981292bf21e0f323bc7
sonihr <huangtianyucn@hotmail.com>
2019-06-25 11:05:00 +0800

    修改了配置文件，已经可以成功运行了。

--- seckill/src/main/java/edu/uestc/MainApp.java
+++ seckill/src/main/java/edu/uestc/MainApp.java
@@ -2,11 +2,13 @@ package edu.uestc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 商品秒杀系统
  */
-
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
